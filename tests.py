3cff922a5be131c0202addaad941765e1aa8c205
Rachel Shorey <rshorey@gmail.com>
2015-08-24 20:37:00 -0400

    tests for find_big_prime)

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 import prime_test as pt
-import find_big_prime as fpb
+import find_big_prime as fbp
 
 class TestPrimeTest(unittest.TestCase):
 
@@ -19,6 +19,23 @@ class TestPrimeTest(unittest.TestCase):
         self.assertTrue(pt.is_it_prime(1999))
         self.assertTrue(pt.is_it_prime(8675309))
 
+    def test_find_big_prime(self):
+        #several times because it's random
+        self.assertIn(fbp.find_big_prime(1), [2,3,5,7])
+        self.assertIn(fbp.find_big_prime(1), [2,3,5,7])
+        self.assertIn(fbp.find_big_prime(1), [2,3,5,7])
+        self.assertIn(fbp.find_big_prime(1), [2,3,5,7])
+        self.assertIn(fbp.find_big_prime(1), [2,3,5,7])
+
+        two_digit_list = [11,13,17,19,23,29,31,
+                          37,41,43,47,53,59,61,67,
+                          71,73,79,83,89,97]
+
+        self.assertIn(fbp.find_big_prime(2), two_digit_list)
+        self.assertIn(fbp.find_big_prime(2), two_digit_list)
+        self.assertIn(fbp.find_big_prime(2), two_digit_list)
+        self.assertIn(fbp.find_big_prime(2), two_digit_list)
+        self.assertIn(fbp.find_big_prime(2), two_digit_list)
 
 if __name__ == '__main__':
     unittest.main()
